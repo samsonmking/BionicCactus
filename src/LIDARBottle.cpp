e9bb2133fb226a7dd6f53f2f8bdc238620ed00de
Sam King <samson@samsonming.com>
2018-01-15 17:38:03 -0500

    LIDAR bottle implementation using polynomial fitting and rolling buffer averaging to get usable data.

---
+++ src/LIDARBottle.cpp
@@ -0,0 +1,34 @@
+#include <LIDARBottle.hpp>
+
+LidarBottle::LidarBottle(): 
+_lox(Adafruit_VL53L0X()), _a0(-47.3), _a1(3.54), _a2(-0.021) {
+    _lox.begin();
+}
+
+void LidarBottle::loop() {
+VL53L0X_RangingMeasurementData_t measure;
+    _lox.rangingTest(&measure, false);
+        if (measure.RangeStatus != 4) {
+        _buffer.push(measure.RangeMilliMeter);
+    }
+}
+
+int LidarBottle::getPercent() {
+    int avg = 0;
+    for(uint i = 0; i < _buffer.size(); i++) {
+        avg += _buffer[i];
+    }
+    avg = avg / _buffer.size();
+    return calculatePercent(avg);
+}
+
+int LidarBottle::calculatePercent(int mm) {
+    int percent = _a0 + (_a1 * mm) + (_a2 * sq(mm));
+    if (percent > 100 ) {
+        return 100;
+    }
+    else if(percent < 0) {
+        return 0;
+    }
+    return percent;
+}
