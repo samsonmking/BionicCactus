c5510352038ea225e7107c6e27e6572464ad3103
samsonmking <samson@samsonmking.com>
2018-05-01 20:36:26 -0400

    Only poll ADC every 100ms to prevent killing WiFi.

--- src/sensors/soil/DFSoil.cpp
+++ src/sensors/soil/DFSoil.cpp
@@ -1,21 +1,40 @@
 #include "DFSoil.hpp"
 
 using namespace Sensors::Soil;
+using namespace Time;
 
-DFSoil::DFSoil(int pin) :
+DFSoil::DFSoil(int pin, Clock &clock) :
   _pin(pin),
+  _clock(clock),
   _high(355),
   _low(709),
   _m(0),
-  _b(0) {
+  _b(0),
+  _lastAverage(0),
+  _lastRead{0} {
   calculateConstants();
 }
 
+void DFSoil::loop() {
+  unsigned long now = _clock.getMillis();
+  if (now - _lastRead < 100) {
+    return;
+  }
+  _buffer.push(readPercent());
+  int sum = 0;
+  for (int i = 0; i < _buffer.size(); i++) {
+    sum += _buffer[i];
+  }
+  _lastAverage = sum / _buffer.size();
+  _lastRead = now;
+}
+
 int DFSoil::getPercent() {
-  return readPercent();
+  return _lastAverage;
 }
 
 int DFSoil::readRaw() {
+
   return analogRead(_pin);
 }
 
--- src/sensors/soil/DFSoil.hpp
+++ src/sensors/soil/DFSoil.hpp
@@ -4,13 +4,15 @@
 #include "SoilSensor.hpp"
 #include "RunningMedian.h"
 #include <Arduino.h>
+#include "time/Clock.hpp"
+#include "CircularBuffer.h"
 
 namespace Sensors {
   namespace Soil {
 
     class DFSoil : public SoilSensor {
     public:
-      DFSoil(int pin);
+      DFSoil(int pin, Time::Clock &clock);
       int getPercent();
       void setHigh(int high) {
         _high = high;
@@ -29,13 +31,18 @@ namespace Sensors {
       int readRaw();
       virtual int readPercent();
       int calculatePercent(int raw);
+      void loop();
     private:
       void calculateConstants();
       int _pin;
+      Time::Clock& _clock;
       int _high;
       int _low;
       float _m;
       float _b;
+      CircularBuffer<int, 10> _buffer;
+      int _lastAverage;
+      unsigned long _lastRead;
     };
 
   }
